8cdbf2c8d3272b867dd0ff98ddf7032806b089bb
watcharaAy <79580552+Watchara-Ay@users.noreply.github.com>
2023-10-31 16:52:38 +0700

    fix container

--- tellme/lib/Components/button.dart
+++ tellme/lib/Components/button.dart
@@ -91,6 +91,14 @@ class ClickableContainer extends StatelessWidget {
                             builder: (context) => const Menudetail()),
                       );
                     },
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.transparent),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ))),
                     child: const Text(
                         "Name       Ckickenwings\nCalories    199  cals\nDistance   199  km\n",
                         style: TextStyle(fontSize: 15)),
@@ -124,17 +132,18 @@ class backbutton extends StatelessWidget {
                   margin: const EdgeInsets.all(10),
                   height: 50.0,
                   // ignore: deprecated_member_use
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: const BorderSide(
-                            color: Color.fromRGBO(0, 0, 0, 1))),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ))),
                     onPressed: () {
                       Navigator.pop(context);
                     },
-                    padding: const EdgeInsets.all(10.0),
-                    color: Colors.black,
-                    textColor: const Color.fromRGBO(255, 255, 255, 1),
                     child: const Text("Back", style: TextStyle(fontSize: 15)),
                   ),
                 ),
